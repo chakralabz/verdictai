9f257a7422952cef8b93bfffd33a8f8913b5d5d2
Ayush Solanki <ayush17solanki@gmail.com>
2026-05-11 19:41:15 +0530

    chore: exposed error handling

--- verdictai/utils/__init__.py
+++ verdictai/utils/__init__.py
@@ -1,5 +1,26 @@
 """Shared utility exports."""
 
+from .errors import (
+    ERROR_DEFINITIONS,
+    DoclingChunkerError,
+    DoclingParserError,
+    ErrorDefinition,
+    VerdictAIError,
+    VerdictAIFileNotFoundError,
+    VerdictAIRuntimeError,
+    VerdictAIValueError,
+)
 from .logger import configure_logging, get_logger
 
-__all__ = ["configure_logging", "get_logger"]
+__all__ = [
+    "ERROR_DEFINITIONS",
+    "DoclingChunkerError",
+    "DoclingParserError",
+    "ErrorDefinition",
+    "VerdictAIError",
+    "VerdictAIFileNotFoundError",
+    "VerdictAIRuntimeError",
+    "VerdictAIValueError",
+    "configure_logging",
+    "get_logger",
+]
